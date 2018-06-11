1400e14217175d93bac352d189af2d801db8c4ea
Diogo Brito Garbin de Souza <diogo.souza@azr-lx-gtw-001.conectcar.dtc>
2018-06-11 12:56:42 -0300

    Scripts Conectcar

---
+++ for_com_array.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -xv
+
+#mkdir -p /home/diogo.souza/migracao
+
+dic=( $(ls /var/rsync/bkp/ | grep lx) )
+dic_length=${#dic[@]}
+echo $dic
+
+
+for i in ${dic[@]}; do
+	if [ ! -d /var/rsync/sysconfig/network-scripts ]; then
+        
+                mkdir -p /home/diogo.souza/migracao/$i
+
+		chmod 755 /home/diogo.souza/migracao/
+
+		cp -prf $p/etc/sysconfig/network-scripts /home/diogo.souza/migracao/$i
+
+	else
+
+		touch /home/diogo.souza/migracao/erro_bkp.txt
+
+		$i >> /home/diogo.souza/migracao/erro_bkp.txt
+
+	fi
+
+done
+
+
+
+
