50cb00f59c4ef865068569dd16ba4b88c83e9e01
diogogarbin <diogogarbin1991@gmail.com>
2018-06-14 17:37:40 -0300

    lista_de_diretorios

--- coleta.py
+++ coleta.py
@@ -1,12 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-info = []
-lista = [ 'IPADDR=' , 'NETMASK=' , 'GATEWAY=' , 'DNS1' ]
-with open ('ifcfg-em1' , 'r') as config:
-	
-	for i in config.readlines():
-	   for elemento in lista:
-	     if (elemento) in i:
-	        info.append(i)
-	print(info)
+import os
+rootdir = '/home/diogo.souza/migracao'
+diretorio = []
+for d in os.scandir('/home/diogo.souza/migracao'):
+    if d.is_dir():
+        diretorio.append(d.path + '\n')
+with open ('lista.tmp', 'a+') as lista:
+    for y in diretorio:
+        lista.write(y)
+
+#info = []
+#lista = [ 'IPADDR=' , 'NETMASK=' , 'GATEWAY=' , 'DNS1' ]
+#with open ('ifcfg-em1' , 'r') as config:
+#	
+#	for i in config.readlines():
+#	   for elemento in lista:
+#	     if (elemento) in i:
+#	        info.append(i)
+#	print(info)
 	
